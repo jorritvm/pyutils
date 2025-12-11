0fdb346630024df97852cb7ae1b715811e67f70f
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2025-12-11 20:23:30 +0100

    adding time utils

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -1,5 +1,16 @@
 from .dicts import merge_dicts, has_empty_leaves
+from .time import extract_iso_date, get_current_date_and_time_str, get_current_date_str, get_current_time_str
 
 __all__ = [
-    "merge_dicts", "has_empty_leaves"
+    "merge_dicts",
+    "has_empty_leaves",
+    "extract_iso_date",
+    "get_current_date_and_time_str",
+    "get_current_date_str",
+    "get_current_time_str",
+    "test"
 ]
+
+
+def test():
+    print("test")
